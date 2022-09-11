84aeaaeeb31b8e88f7c26fb3d458329fa6caa398
eniola-ajala <ajalaeniola454@gmail.com>
2022-09-11 10:55:19 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,25 @@
-#include<unistd.h>
+
+#include <stdio.h>
+
 
 /**
- *
- *  *  * main - prints the standrad error sandwiched around the  standard outpu/t
- *
- *   *   *
- *
- *    *    * Return: returns 1
- *
- *     */
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
 
 int main(void)
-
 {
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
-
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
