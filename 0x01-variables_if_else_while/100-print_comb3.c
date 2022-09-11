31fcc1b26a391fa2381b04f36d66573f05836ae3
eniola-ajala <ajalaeniola454@gmail.com>
2022-09-11 18:56:08 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,61 +1,31 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   * Description: prints all possible different combinations of two digits
- *
- *    * Return: Always return (0) success
- *
- *     */
-
+ * main - Entry point
+ * Description: prints all possible different combinations of two digits
+ * Return: Always return (0) success
+ */
 int main(void)
-
 {
+	int c, v;
 
-		int c, v;
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (v = '0'; v <= '9'; v++)
+		{
+			if (c < v)
+			{
+				putchar(c);
+				putchar(v);
 
-
-
-			for (c = '0'; c <= '9'; c++)
-
+					if (c != '8' || (c == '8' && v != '9'))
 					{
-
-								for (v = '0'; v <= '9'; v++)
-
-											{
-
-															if (c < v)
-
-																			{
-
-																								putchar(c);
-
-																												putchar(v);
-
-
-
-																																	if (c != '8' || (c == '8' && v != '9'))
-
-																																							{
-
-																																														putchar(',');
-
-																																																				putchar(' ');
-
-																																																									}
-
-																																				}
-
-																	}
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
